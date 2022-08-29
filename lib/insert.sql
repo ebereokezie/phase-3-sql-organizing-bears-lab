3a3cf0ba29495edffd9c6e96170101c2c0b0549e
Ebere Okezie <ebereokezie@gmail.com>
2022-08-29 16:08:03 -0500

    commit tree 29cfa2ee9c22e31c887d43993d43d9833b01183b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 32, "M", "Brown", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 31, "M", "Black", "Crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 28, "F", "Brown", "Patient", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 45, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 30, "F", "Brown", "Energetic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 36, "M", "Black", "Angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 29, "F", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
